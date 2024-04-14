a53b2be11ab1b2061e6a6d9e39c4b423aa180c4b
Jacob Huang <objecthuang@hotmail.com>
2024-04-14 15:11:02 +0800

    update configure to support livy submit spark cluster

--- configs/zeppelin/zeppelin-env.sh
+++ configs/zeppelin/zeppelin-env.sh
@@ -1,4 +1,5 @@
 export SPARK_HOME=/usr/local/spark
 export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop
+export SPARK_SUBMIT_OPTIONS="--conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
 
 
